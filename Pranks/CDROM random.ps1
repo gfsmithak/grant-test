c34bd59e085c8a025e92c8f2568857d8f547e010
gfsmithak <gfsmithak@gmail.com>
2016-05-26 16:45:11 -0800

    Rename CDROM random.ps1 to Pranks/CDROM random.ps1

---
+++ Pranks/CDROM random.ps1
@@ -0,0 +1,13 @@
+﻿powershell.exe -noexit -windowstyle hidden {
+$sh = New-Object -ComObject "Shell.Application"
+$timer=new-object system.timers.timer
+$timer.interval = 100
+$timer.autoreset=$true
+register-objectevent -inputobject $timer -eventname elapsed -action {
+$number = (get-random -minimum "1" -maximum "10")
+if ($number -gt "5"){$sh.Namespace(17).Items() |  Where-Object { $_.Type -eq "CD Drive" } | foreach { $_.InvokeVerb("Eject") }}
+
+}
+
+$timer.enabled=$true
+}
