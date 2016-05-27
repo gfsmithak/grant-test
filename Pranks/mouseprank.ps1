128e1a30951bd24578c1617c29f3f04c9638b987
gfsmithak <gfsmithak@gmail.com>
2016-05-26 16:45:38 -0800

    Rename mouseprank.ps1 to Pranks/mouseprank.ps1

--- Pranks/mouseprank.ps1
+++ Pranks/mouseprank.ps1
@@ -2,13 +2,13 @@ powershell.exe -noexit -windowstyle hidden {
 $CSharpSig = @'
 [DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
 public static extern bool SystemParametersInfo(
-                 uint uiAction,
-                 uint uiParam,
-                 uint pvParam,
-                 uint fWinIni);
+Â Â  Â Â Â Â Â Â Â Â  Â Â Â Â Â uint uiAction,
+Â Â Â  Â Â Â Â Â Â Â  Â Â Â Â Â uint uiParam,
+Â Â Â  Â Â Â Â Â Â Â  Â Â Â Â Â uint pvParam,
+Â Â  Â Â Â Â Â Â Â Â  Â Â Â Â Â uint fWinIni);
 '@
 
-$mousespeed= Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo –PassThru
+$mousespeed= Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo â€“PassThru
 
 while ($true){
 $rnd=get-random -minimum 1 -maximum 20
